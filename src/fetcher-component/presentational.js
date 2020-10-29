bebbf2ed8b1d06320f6b8540669d276aa0edee0b
dpMelian <astrodaniel1234@gmail.com>
2020-10-29 12:45:19 +0000

    Added footer with credits

--- src/fetcher-component/presentational.js
+++ src/fetcher-component/presentational.js
@@ -11,20 +11,30 @@ export default class FetchRender extends Component {
       )
     } else {
       return (
-        <Container>
-          <h1>NASA Astronomy Picture Of the Day</h1>
-          <Card className="mx-auto" style={{ width: '36rem' }}>
-            <Card.Img alt="nasa-apod" variant="top" src={ this.props.data.url } />
-            <Card.Body>
-              <p><strong> { this.props.data.title }</strong></p>
-              { this.props.dataauthor && <p><small>Author: { this.props.data.author }</small></p> }
-              { this.props.data.date && <p><small>Date: { this.props.data.date }</small></p> }
-              <p><em>{ this.props.data.description }</em></p>
-              <p><Button className="mr-2" variant="primary" onClick={ this.props.handle }>Random APOD</Button>
-              <Button href={ this.props.data.hdurl } target="_blank" variant="primary">View HD version</Button></p>
-            </Card.Body>
-          </Card>
-        </Container>
+        <div>
+          <Container>
+            <h1>NASA Astronomy Picture Of the Day</h1>
+            <Card className="mx-auto" style={{ width: '36rem' }}>
+              <Card.Img alt="nasa-apod" variant="top" src={ this.props.data.url } />
+              <Card.Body>
+                <p><strong> { this.props.data.title }</strong></p>
+                { this.props.dataauthor && <p><small>Author: { this.props.data.author }</small></p> }
+                { this.props.data.date && <p><small>Date: { this.props.data.date }</small></p> }
+                <p><em>{ this.props.data.description }</em></p>
+                <p><Button className="mr-2" variant="primary" onClick={ this.props.handle }>Random APOD</Button>
+                <Button href={ this.props.data.hdurl } target="_blank" variant="primary">View HD version</Button></p>
+              </Card.Body>
+            </Card>
+          </Container>
+
+          <footer className="footer p-3 mt-3 mb-3 bg-light">
+            <Container>
+              <span>Created using <a href="https://reactjs.org/">React </a>
+                by <a href="https://github.com/dpMelian">dpMelian</a> using
+                <a href="https://api.nasa.gov/"> NASA's public API</a>.</span>
+            </Container>
+          </footer>
+        </div>
       )
     }
   }
